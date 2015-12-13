c3f3f48129a25099e7ec0c462beeccc564c5dad4
svoig <harrisonccole@gmail.com>
2015-12-13 12:55:34 -0800

    Fixed the non-random deck generator! Properly marks vowels. Also added the missing three Russian letters

--- app/public/javascripts/models/cards.js
+++ app/public/javascripts/models/cards.js
@@ -15,9 +15,9 @@ var engLetters = Backbone.Collection.extend({
 });
 
 //MY data for the cards. Used to generate randomized decks (eventually)
-var rusArray = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Э','Ю','Я'];
+var rusArray = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы', 'Ь','Э','Ю','Я'];
 
-var engArray = ['A','B','V','G','D','Ye','Yo','Zh','Z','I','Y','K','L','M','N','O','P','R','S','T','U','F','Kh','Ts','Ch','Sh','Shch','Yu','Ya'];
+var engArray = ['A','B','V','G','D','Ye','Yo','Zh','Z','I','Y','K','L','M','N','O','P','R','S','T','U','F','Kh','Ts','Ch','Sh','Shch','Hard sign', 'Ui', 'Soft sign','E','Yu','Ya'];
 
 //THe actual card constructor. RIght now it should return a new Card with a value, id, and vowel properties needed to start the game
 var Card = Backbone.Model.extend(
@@ -27,54 +27,57 @@ var Card = Backbone.Model.extend(
 
 function generateDeck() {
   //Initialize vowel arrays to determine the vowel property of the cards.
-  var rusVowels = ['А','Я','Э','Е','У','Ю','О','Ё','И'];
-  var engVowels = ['A', 'Ya', 'E', 'Ye','U','Yu','O','Yo','I'];
+  var rusVowels = ['А','Я','Э','Е','У','Ю','О','Ё','И','Ы'];
+  var engVowels = ['A', 'Ya', 'E', 'Ye','U','Yu','O','Yo','I','Ui'];
   //Give the cards a place to go
   var deck = {};
   //To make sure all card ids are unique
   var counter =0;
 
+//Loop through both arrays, comparing each letter with the vowels
   rusArray.forEach(function(key1){
 
     rusVowels.forEach(function(key2) {
+      //If the first letter is a vowel, make a new card with vowel: true
       if (key1 === key2) {
         var newCard = new Card({vowel: true, id: counter, value: key1});
+        //Increment the counter, and add the new card to the deck
         counter++;
 
         deck[newCard.id] = newCard;
       }
 
     });
-
-    if (!(key1 in deck)) {
-      console.log("Didn't find ",key1," in Russian deck. Adding!");
+    //If the last card added was a vowel, don't add it again. If it's not a vowel, make a new card for the letter and set vowel: false
+    if (!(deck[counter-1].get('value') === key1)) {
       var newCard = new Card({id: counter, value: key1});
       deck[newCard.id]  = newCard;
 
       counter++;
-    } else console.log(key, " already in Russian deck. Skipping!");
+    }
 
   });
-
+//Loop through both arrays, comparing each letter with the vowels
   engArray.forEach(function(key1){
 
     engVowels.forEach(function(key2) {
+      //If the first letter is a vowel, make a new card with vowel: true
       if (key1 === key2) {
         var newCard = new Card({vowel: true, id: counter, value: key1});
+        //Increment the counter, and add the new card to the deck
         counter++;
 
         deck[newCard.id] = newCard;
       }
 
     });
-
-    if (!(key1 in deck)) {
-      console.log("Didn't find ",key1," in English deck. Adding!");
+    //If the last card added was a vowel, don't add it again. If it's not a vowel, make a new card for the letter and set vowel: false
+    if (!(deck[counter-1].get('value') === key1)) {
       var newCard = new Card({id: counter, value: key1});
       deck[newCard.id]  = newCard;
 
       counter++;
-    } else console.log(key, " already in English deck. Skipping!");
+    }
 
   });
 
